ba105adbe30c2924c8e204025f9bfbe2ab6fcd70
Ashraf Mollah <ashraf@xpeedstudio.com>
2022-01-11 01:30:30 +0600

    fixed star missing

--- src/widgets/star-3-widget.js
+++ src/widgets/star-3-widget.js
@@ -16,27 +16,28 @@ export default class StarWidget3 extends ControlBase {
     registerControls() {
         
         this.addControl('star_color', {
-            label: 'Star Color',
+            label: 'Star 3 Color',
             type: 'color',
             default: '#c75943',
             isLabelInline: true,
             selector: function() {
-                return `#star_widget3 span {
-                    color: ${this.default};
+                return `#star_widget3 path {
+                    fill: ${this.default};
                 }`
             }
         })
 
         this.addControl('star_size', {
-            label: 'Star Size',
+            label: 'Star 3 Size',
             type: 'slider',
             default: 44,
             max: 200,
             step:1,
             selector: function() {
-                return `#star_widget3 span {
+                return `#star_widget3 svg {
                     display: inline-block;
-                    font-size: ${this.default}px;
+                    width: ${this.default}px;
+                    height: ${this.default}px;
                 }`
             }
         })
@@ -47,7 +48,9 @@ export default class StarWidget3 extends ControlBase {
             <div class="popup-widget-element" id="star_widget3" data-type="widget">
                 <i class="remove-btn">x</i>
                 <div>
-                    <span class="popup-star"></span>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                        <path fill="#12fbec" d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"></path>
+                    </svg>
                 </div>
             </div>
         `
--- src/widgets/star-widget.js
+++ src/widgets/star-widget.js
@@ -16,27 +16,28 @@ export default class StarWidget extends ControlBase {
     registerControls() {
         
         this.addControl('star_color', {
-            label: 'Star Color',
+            label: 'Star 1 Color',
             type: 'color',
             default: '#c75943',
             isLabelInline: true,
             selector: function() {
-                return `#star_widget span {
-                    color: ${this.default};
+                return `#star_widget path {
+                    fill: ${this.default};
                 }`
             }
         })
 
         this.addControl('star_size', {
-            label: 'Star Size',
+            label: 'Star 1 Size',
             type: 'slider',
             default: 44,
             max: 200,
             step:1,
             selector: function() {
-                return `#star_widget span {
+                return `#star_widget svg {
                     display: inline-block;
-                    font-size: ${this.default}px;
+                    width: ${this.default}px;
+                    height: ${this.default}px;
                 }`
             }
         })
@@ -47,7 +48,9 @@ export default class StarWidget extends ControlBase {
             <div class="popup-widget-element" id="star_widget" data-type="widget">
                 <i class="remove-btn">x</i>
                 <div>
-                    <span class="popup-star"></span>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+                        <path fill="#12fbec" d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"></path>
+                    </svg>
                 </div>
             </div>
         `
