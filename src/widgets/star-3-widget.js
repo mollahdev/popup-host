e5a11be04fd74473a4639c5d07e5d3c02c4c1a4e
Ashraf Mollah <ashraf@xpeedstudio.com>
2022-01-11 01:42:52 +0600

    Revert "fixed star missing"
    This reverts commit ba105adbe30c2924c8e204025f9bfbe2ab6fcd70.

--- src/widgets/star-3-widget.js
+++ src/widgets/star-3-widget.js
@@ -16,28 +16,27 @@ export default class StarWidget3 extends ControlBase {
     registerControls() {
         
         this.addControl('star_color', {
-            label: 'Star 3 Color',
+            label: 'Star Color',
             type: 'color',
             default: '#c75943',
             isLabelInline: true,
             selector: function() {
-                return `#star_widget3 path {
-                    fill: ${this.default};
+                return `#star_widget3 span {
+                    color: ${this.default};
                 }`
             }
         })
 
         this.addControl('star_size', {
-            label: 'Star 3 Size',
+            label: 'Star Size',
             type: 'slider',
             default: 44,
             max: 200,
             step:1,
             selector: function() {
-                return `#star_widget3 svg {
+                return `#star_widget3 span {
                     display: inline-block;
-                    width: ${this.default}px;
-                    height: ${this.default}px;
+                    font-size: ${this.default}px;
                 }`
             }
         })
@@ -48,9 +47,7 @@ export default class StarWidget3 extends ControlBase {
             <div class="popup-widget-element" id="star_widget3" data-type="widget">
                 <i class="remove-btn">x</i>
                 <div>
-                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-                        <path fill="#12fbec" d="M32 12.408l-11.056-1.607-4.944-10.018-4.944 10.018-11.056 1.607 8 7.798-1.889 11.011 9.889-5.199 9.889 5.199-1.889-11.011 8-7.798z"></path>
-                    </svg>
+                    <span class="popup-star"></span>
                 </div>
             </div>
         `
